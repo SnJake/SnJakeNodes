6bf62aac6245629e4c4dfaeced0aa2b0718b9788
SnJake <66511504+SnJake@users.noreply.github.com>
2025-07-18 16:55:10 +0400

    Update teleport_nodes.py

--- teleport_nodes.py
+++ teleport_nodes.py
@@ -1,34 +1,30 @@
 class SnJake_TeleportSet:
     """
     Python-заглушка для виртуального узла Teleport Set.
-    Вся логика находится в /js/snjake_teleport_ui.js
+    Вся логика реализована в /js/snjake_teleport_ui.js
     """
     CATEGORY = "😎 SnJake/Utils"
     FUNCTION = "do_nothing"
-    INPUT_IS_LIST = True
-    OUTPUT_IS_LIST = (True,)
 
     @classmethod
     def INPUT_TYPES(cls):
-        return {"required": {}}
+        return {"required": {}} # JS добавит входы динамически
 
     def do_nothing(self, **kwargs):
-        return (list(kwargs.values()),)
+        return ()
 
 
 class SnJake_TeleportGet:
     """
     Python-заглушка для виртуального узла Teleport Get.
-    Вся логика находится в /js/snjake_teleport_ui.js
+    Вся логика реализована в /js/snjake_teleport_ui.js
     """
     CATEGORY = "😎 SnJake/Utils"
     FUNCTION = "do_nothing"
-    INPUT_IS_LIST = True
-    OUTPUT_IS_LIST = (True,)
 
     @classmethod
     def INPUT_TYPES(cls):
-        return {"required": {}}
+        return {"required": {}} # JS добавит входы динамически
 
     def do_nothing(self, **kwargs):
-        return ([],)
+        return ()
