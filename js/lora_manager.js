b2cb138a9f1aca3308ccfcf18d768a8680d2231f
SnJake <66511504+SnJake@users.noreply.github.com>
2025-08-15 19:57:31 +0400

    Update lora_manager.js

--- js/lora_manager.js
+++ js/lora_manager.js
@@ -269,15 +269,3 @@ function moveWidgetToEnd(node, widget) {
 
 function clamp(v, a, b){ return Math.max(a, Math.min(b, v)); }
 
-
-app.registerExtension({
-  name: "SnJake.LoRAManagerWithPreview.Color",
-  nodeCreated(node) {
-    if (node.comfyClass === "LoRAManagerWithPreview") {
-      node.color   = "#2e2e36";
-      node.bgcolor = "#41414a";
-      node.boxcolor = "#555A"; // опционально
-      node.setDirtyCanvas(true, true);
-    }
-  }
-});
